e23102b9baa386adf3183e171f79837bd82883ce
Henrique-Givisiez <lucogds@gmail.com>
2023-11-29 17:54:58 -0300

    Sexto commit

--- static/JavaScript/menu_dot.js
+++ static/JavaScript/menu_dot.js
@@ -1,21 +1,32 @@
 // Função para mostrar/ocultar o dropdown
 function toggleDropdown(btn) {
     var parent_div = btn.parentElement;
-    alert(parent_div.classList); 
     var dropdown = parent_div.querySelector("#myDropdown");
     dropdown.classList.toggle("show");
   }
   
-  // Fechar o dropdown se o usuário clicar fora dele
   window.onclick = function(event) {
-    if (!event.target.matches('button')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      for (var i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
+    var buttons = document.querySelectorAll('button.menu');
+
+    // Verifica se o alvo do clique não é nenhum dos botões e não é um elemento SVG dentro desses botões
+    if (!isButtonOrChild(event.target, buttons)) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
         }
-      }
     }
-  }
-  
+}
+
+// Função auxiliar para verificar se o elemento clicado é um botão ou está dentro de um botão
+function isButtonOrChild(element, buttons) {
+    for (var i = 0; i < buttons.length; i++) {
+        var button = buttons[i];
+        if (element === button || button.contains(element)) {
+            return true;
+        }
+    }
+    return false;
+}
