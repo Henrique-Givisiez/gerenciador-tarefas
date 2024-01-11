ec287347d49a45080bf36703ef9f1370c0afa6f8
henrique-givisiez <lucogds@gmail.com>
2024-01-11 12:02:45 -0300

    update helpers

--- app/auth/helper.py
+++ app/auth/helper.py
@@ -1,8 +1,14 @@
 from hashlib import sha256
 from re import match
 from app.database.base import BaseHelper
+from app.tasks.helper import TaskHelper
+import pymysql
 
+# CRUD accounts
 class AccountsHelper(BaseHelper):
+    def __init__(self, connection: pymysql.Connection, cursor: pymysql.cursors.Cursor):
+        super().__init__(connection, cursor)
+        self.task = TaskHelper()
 
     # Check login
     def check_auth(self, email: str, password: str):
@@ -34,6 +40,13 @@ class AccountsHelper(BaseHelper):
             return msg, success
     
 
+    # Check if user have tasks
+    def user_have_tasks(self, user_id):
+        result = self.task.read(user_id=user_id)
+        if result:
+            return True
+        return False
+    
         
     # Create a new user function
     def create(self, username: str, email: str, password: str):
@@ -79,6 +92,7 @@ class AccountsHelper(BaseHelper):
             msg = f"Ocorreu um erro: {error}"
             return success, msg
     
+
     # Read user 
     def read(self, user_id: int):
         query_select_contas = "SELECT * FROM contas WHERE id = %s"
@@ -88,3 +102,62 @@ class AccountsHelper(BaseHelper):
         except Exception as error:
             return f"Ocorreu um erro: {error}"
     
+
+    # Update user
+    def update(self, user_id: int, new_username: str = None, new_email: str = None, new_password: str = None):
+        success = False
+        fields_list = []
+        new_values = []
+
+        if new_username:
+            fields_list.append("usuario = %s")
+            new_values.append(new_username)
+
+        if new_email:
+            fields_list.append("email = %s")
+            new_values.append(new_email)
+
+        if new_password:
+            hashed_password = sha256(new_password.encode()).hexdigest()
+            fields_list.append("senha = %s")
+            new_values.append(hashed_password)
+
+        # Join all existing fields to update query
+        query_update_contas = "UPDATE contas SET" + ", ".join(fields_list) + "WHERE id = %s"
+        new_values.append(user_id)
+
+        try:
+            # Execute the query
+            self.cursor.execute(query_update_contas, tuple(new_values))
+            self.conn.commit()
+            success = True
+            return success
+
+        except Exception as error:
+            print(f"Ocorreu um erro: {error}")
+            self.conn.rollback()
+            return success
+        
+    
+    # Delete user
+    def delete(self, user_id: int):
+        query_delete_contas = "DELETE FROM contas WHERE id = %s"
+        success = False
+        msg = ""
+
+        try:
+            # Call function "user_have_tasks" to check tasks. If true, delete tasks from user 
+            if not self.user_have_tasks(user_id):
+                self.cursor.execute(query_delete_contas, (user_id))
+                self.conn.commit()
+                success = True
+                return success
+        
+            else:
+                self.task.delete(user_id=user_id)
+                return self.delete(user_id)
+            
+        except Exception as error:
+            msg = f"Ocorreu um erro {error}"
+            self.conn.rollback()
+            return msg, success
---
+++ app/tasks/helper.py
@@ -0,0 +1,112 @@
+from app.database.base import BaseHelper
+
+# CRUD for tasks
+class TaskHelper(BaseHelper):
+
+    # Create Task
+    def create(self, user_id: int, task_type: str, task_description: str, task_date: str):
+        success = False
+
+        task_status = "pendente"
+
+        query_insert_tasks = """INSERT INTO tarefas(
+        usuario_id, categoria_tarefa, descricao_tarefa, data_tarefa, status_tarefa)
+        VALUES (%s, %s, %s, %s, %s)"""
+
+        try:
+            if task_type and task_date: # Required fields
+                self.cursor.execute(query_insert_tasks, (user_id, task_type, task_description, task_date, task_status))
+                self.conn.commit()
+                success = True
+                return success
+            
+            else:
+                msg = "Campos incompletos!"
+                return msg, success
+                        
+        
+        except Exception as error:
+            msg = f"Ocorreu um erro: {error}"
+            return msg, success
+        
+
+    # Read task
+    def read(self, task_id: int = None, user_id: int = None):
+        try:
+            if task_id:
+                query_read_task = "SELECT * FROM tarefas WHERE id = %s"
+                self.cursor.execute(query_read_task, (task_id))
+                task = self.cursor.fetchone()
+                return task
+            
+            if user_id:
+                query_read_tasks_by_user = "SELECT * FROM tarefas WHERE usuario_id = %s"
+                self.cursor.execute(query_read_tasks_by_user, (user_id))
+                user_tasks = self.cursor.fetchall()
+                return user_tasks
+
+        except Exception as error:
+            msg = f"Ocorreu um erro: {error}"
+            return msg  
+        
+    
+    # Update task
+    def update(self, user_id: int, new_task_type: str = None, new_task_descriprion: str = None, new_task_date: str = None, new_task_status: str = None):
+        success = False
+        fields_list = []
+        new_values = []
+
+        if new_task_type:
+            fields_list.append("categoria_tarefa= %s")
+            new_values.append(new_task_type)
+
+        if new_task_descriprion:
+            fields_list.append("descricao_tarefa = %s")
+            new_values.append(new_task_descriprion)
+
+        if new_task_date:
+            fields_list.append("data_tarefa = %s")
+            new_values.append(new_task_date)
+
+        if new_task_status:
+            fields_list.append("status_tarefa = %s")
+            new_values.append(new_task_status)
+
+        # Join all existing fields to update query
+        query_update_contas = "UPDATE contas SET" + ", ".join(fields_list) + "WHERE id = %s"
+        new_values.append(user_id)
+
+        try:
+            # Execute the query
+            self.cursor.execute(query_update_contas, tuple(new_values))
+            self.conn.commit()
+            success = True
+            return success
+
+        except Exception as error:
+            msg = f"Ocorreu um erro: {error}"
+            self.conn.rollback()
+            return success, msg
+        
+
+    # Delete task
+    def delete(self, task_id: int = None, user_id: int = None):
+        success = False
+
+        try:
+            if task_id:
+                query_delete_task = "DELETE FROM tarefas WHERE id = %s"
+                self.cursor.execute(query_delete_task, (task_id))
+            
+            if user_id:
+                query_delete_user_tasks = "DELETE FROM tarefa WHERE usuario_id = %s"
+                self.cursor.execute(query_delete_user_tasks, (user_id))
+            
+            self.conn.commit()
+            success = True
+            return success
+        
+        except Exception as error:
+            self.conn.rollback()
+            msg = f"Ocorreu um erro: {error}"
+            return msg, success
